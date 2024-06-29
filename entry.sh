9959408821e821685c675a0ffb06c8a4b981fa66
Abner <22141172+Silentely@users.noreply.github.com>
2024-06-29 10:52:58 +0800

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -1,4 +1,6 @@
-cd /telegram-bot-asf
-nohup python3 telegram-asf.py tgbot.toml > /dev/null 2>&1 &
-cd -
-bash /app/ArchiSteamFarm.sh --process-required --service || bash /app/ArchiSteamFarm-Service.sh  --process-required --service
+#!/bin/bash
+# 激活虚拟环境
+source /telegram-bot-asf/venv/bin/activate
+# 运行你的Python脚本
+nohup python /telegram-bot-asf/telegram-asf.py tgbot.toml > /dev/null 2>&1 &
+bash /asf/ArchiSteamFarm-Service.sh  --process-required --service
